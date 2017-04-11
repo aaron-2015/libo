b2a18ba32076fce8eecfb0832abf9b33226164d3
aaron <aaron@aarondeMac-mini.local>
2017-04-11 17:57:13 +0800

    添加网络监测

--- libo/ViewController.m
+++ libo/ViewController.m
@@ -8,30 +8,109 @@
 
 
 #import "ViewController.h"
+#import "AFNetworkReachabilityManager.h"
 
 static NSString *urlString = @"http://libo.toumaps.com/index.html";
 
-@interface ViewController ()
+@interface ViewController ()<UIWebViewDelegate,UIAlertViewDelegate>
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (strong, nonatomic) AFNetworkReachabilityManager *afnetworkReachabilityManager;
+@property (strong, nonatomic) UIAlertView *alertView;
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    _webView.delegate = self;
+    _afnetworkReachabilityManager = [AFNetworkReachabilityManager sharedManager];
+    
+    __weak typeof(self) weakSelf = self;
+    [_afnetworkReachabilityManager setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        
+        NSLog(@"%ld",(long)status);
+        
+        switch (status) {
+            case AFNetworkReachabilityStatusUnknown:
+            case AFNetworkReachabilityStatusNotReachable:
+            {
+                [weakSelf.alertView show];
+            }
+                break;
+            case AFNetworkReachabilityStatusReachableViaWiFi:
+            case AFNetworkReachabilityStatusReachableViaWWAN:
+            {
+                [weakSelf.afnetworkReachabilityManager stopMonitoring];
+                [weakSelf.alertView dismissWithClickedButtonIndex:0 animated:YES];
+                
+                NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:urlString]];
+                [weakSelf.webView loadRequest:request];
+            }
+                break;
+            default:
+                break;
+        }
+    }];
+    
+    [_afnetworkReachabilityManager startMonitoring];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    _webView = nil;
+    [self cleanCacheAndCookie];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex) {
         NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:urlString]];
-        [_webView loadRequest:request];
-    });
+        [self.webView loadRequest:request];
+    }
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (UIAlertView *)alertView
+{
+    if (!_alertView) {
+        _alertView = [[UIAlertView alloc] initWithTitle:@"提示" message:@"产业监测需要您授权使用数据,如已授权，请点击重试，或者关掉app重新打开！" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"重试", nil];
+    }
+    return _alertView;
+}
+
+/**清除缓存和cookie*/
+
+- (void)cleanCacheAndCookie{
+    
+    //清除cookies
+    
+    NSHTTPCookie *cookie;
+    
+    NSHTTPCookieStorage *storage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+    
+    for (cookie in [storage cookies]){
+        
+        [storage deleteCookie:cookie];
+        
+    }
+    
+    //清除UIWebView的缓存
+    
+    [[NSURLCache sharedURLCache] removeAllCachedResponses];
+    
+    NSURLCache * cache = [NSURLCache sharedURLCache];
+    
+    [cache removeAllCachedResponses];
+    
+    [cache setDiskCapacity:0];
+    
+    [cache setMemoryCapacity:0];
+    
 }
 
 
