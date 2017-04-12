78e884e2fc8e0fc83928ffdbcfaaa54ae1598156
aaron <aaron@aarondeMac-mini.local>
2017-04-12 13:08:21 +0800

    添加网络监测

--- libo/ViewController.m
+++ libo/ViewController.m
@@ -9,13 +9,15 @@
 
 #import "ViewController.h"
 #import "AFNetworkReachabilityManager.h"
+#import "SVProgressHUD.h"
 
 static NSString *urlString = @"http://libo.toumaps.com/index.html";
 
 @interface ViewController ()<UIWebViewDelegate,UIAlertViewDelegate>
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 @property (strong, nonatomic) AFNetworkReachabilityManager *afnetworkReachabilityManager;
-@property (strong, nonatomic) UIAlertView *alertView;
+//@property (strong, nonatomic) UIAlertView *alertView;
+@property (strong, nonatomic) NSTimer *timer;
 
 @end
 
@@ -26,37 +28,56 @@ static NSString *urlString = @"http://libo.toumaps.com/index.html";
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+     [self cleanCacheAndCookie];
     _webView.delegate = self;
     _afnetworkReachabilityManager = [AFNetworkReachabilityManager sharedManager];
-    
-    __weak typeof(self) weakSelf = self;
-    [_afnetworkReachabilityManager setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
-        
-        NSLog(@"%ld",(long)status);
-        
-        switch (status) {
-            case AFNetworkReachabilityStatusUnknown:
-            case AFNetworkReachabilityStatusNotReachable:
-            {
-                [weakSelf.alertView show];
-            }
-                break;
-            case AFNetworkReachabilityStatusReachableViaWiFi:
-            case AFNetworkReachabilityStatusReachableViaWWAN:
-            {
-                [weakSelf.afnetworkReachabilityManager stopMonitoring];
-                [weakSelf.alertView dismissWithClickedButtonIndex:0 animated:YES];
-                
-                NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:urlString]];
-                [weakSelf.webView loadRequest:request];
-            }
-                break;
-            default:
-                break;
-        }
-    }];
-    
     [_afnetworkReachabilityManager startMonitoring];
+    
+    if (_afnetworkReachabilityManager.isReachable) {
+        [SVProgressHUD dismiss];
+        NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:urlString]];
+        [self.webView loadRequest:request];
+    } else {
+        _timer = [NSTimer timerWithTimeInterval:0.3 target:self selector:@selector(checkAndLoad) userInfo:nil repeats:YES];
+        [[NSRunLoop mainRunLoop] addTimer:_timer forMode:NSDefaultRunLoopMode];
+    }
+    
+//    __weak typeof(self) weakSelf = self;
+//    [_afnetworkReachabilityManager setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+//        
+//        NSLog(@"%ld",(long)status);
+//        
+//        switch (status) {
+//            case AFNetworkReachabilityStatusUnknown:
+//            case AFNetworkReachabilityStatusNotReachable:
+//            {
+////                [weakSelf.alertView show];
+//            }
+//                break;
+//            case AFNetworkReachabilityStatusReachableViaWiFi:
+//            case AFNetworkReachabilityStatusReachableViaWWAN:
+//            {
+//                [weakSelf.afnetworkReachabilityManager stopMonitoring];
+//                [weakSelf.alertView dismissWithClickedButtonIndex:0 animated:YES];
+//                
+//                NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:urlString]];
+//                [weakSelf.webView loadRequest:request];
+//            }
+//                break;
+//            default:
+//                break;
+//        }
+//    }];
+    
+//    [_afnetworkReachabilityManager startMonitoring];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    if (!_afnetworkReachabilityManager.isReachable) {
+        [SVProgressHUD showWithStatus:@"正在加载中..." maskType:SVProgressHUDMaskTypeClear];
+    }
 }
 
 - (void)viewWillDisappear:(BOOL)animated
@@ -66,51 +87,54 @@ static NSString *urlString = @"http://libo.toumaps.com/index.html";
     [self cleanCacheAndCookie];
 }
 
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+- (void)checkAndLoad
 {
-    if (buttonIndex) {
+    NSLog(@"%ld",(long)_afnetworkReachabilityManager.networkReachabilityStatus);
+    
+    if (_afnetworkReachabilityManager.isReachable) {
+        
+        [_timer invalidate];
+        _timer = nil;
+        
+        [SVProgressHUD dismiss];
         NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:urlString]];
         [self.webView loadRequest:request];
     }
 }
 
-
-- (UIAlertView *)alertView
-{
-    if (!_alertView) {
-        _alertView = [[UIAlertView alloc] initWithTitle:@"提示" message:@"产业监测需要您授权使用数据,如已授权，请点击重试，或者关掉app重新打开！" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"重试", nil];
-    }
-    return _alertView;
-}
+//- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+//{
+//    if (buttonIndex) {
+//        NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:urlString]];
+//        [self.webView loadRequest:request];
+//    }
+//}
+//
+//
+//- (UIAlertView *)alertView
+//{
+//    if (!_alertView) {
+//        _alertView = [[UIAlertView alloc] initWithTitle:@"提示" message:@"产业监测需要您授权使用数据,如已授权，请点击重试，或者关掉app重新打开！" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"重试", nil];
+//    }
+//    return _alertView;
+//}
 
 /**清除缓存和cookie*/
-
 - (void)cleanCacheAndCookie{
     
     //清除cookies
-    
     NSHTTPCookie *cookie;
-    
     NSHTTPCookieStorage *storage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
-    
     for (cookie in [storage cookies]){
-        
         [storage deleteCookie:cookie];
-        
     }
     
     //清除UIWebView的缓存
-    
     [[NSURLCache sharedURLCache] removeAllCachedResponses];
-    
     NSURLCache * cache = [NSURLCache sharedURLCache];
-    
     [cache removeAllCachedResponses];
-    
     [cache setDiskCapacity:0];
-    
     [cache setMemoryCapacity:0];
-    
 }
 
 
